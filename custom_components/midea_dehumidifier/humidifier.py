a2953cb341283cfbc7abd69f64e3934efb9ac581
Dzhunet Hasan <dzhunet.hasan@adevinta.com>
2024-05-07 16:03:03 +0200

    - Change dependency

--- custom_components/midea_dehumidifier/humidifier.py
+++ custom_components/midea_dehumidifier/humidifier.py
@@ -4,7 +4,7 @@ For more details please refer to the documentation at
 https://github.com/barban-dev/midea_inventor_dehumidifier
 """
 LOG_TAG = "midea_dehumidifier"
-VERSION = '1.0.7'
+VERSION = '1.0.0'
 
 import logging
 
